78695464461a427f4e4ee472f6cbb14f0f07ac98
Allen Amaya <ahlee.amaya@gmail.com>
2026-06-04 10:58:01 +0300

    Allow Vercel Frontend in CORS

--- backend/kenwell_erp/settings.py
+++ backend/kenwell_erp/settings.py
@@ -150,6 +150,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://kenwell-erp-system.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
